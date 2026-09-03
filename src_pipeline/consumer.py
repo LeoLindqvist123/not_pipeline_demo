646359adaae8b015a5767f9fd4431b071ca49307
Leo Lindqvist Kröhnert <leolikro01@gmail.com>
2026-09-03 18:10:47 +0200

    postgres

--- src_pipeline/consumer.py
+++ src_pipeline/consumer.py
@@ -4,12 +4,16 @@ import json
 def on_message(client, userdata, message):
     # needs to decode because original msg is encoded after publshing
     payload = message.payload.decode()
+    data = json.loads(payload)
 
-    print(payload)
+    temperature = float(data["temperature"])
+    humidity = float(data["humidity"])
+    # code for storing data in to timescaledb
+    print(temperature, humidity)
 
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     client = mqtt.Client()
     client.connect("localhost", 1883)
     client.subscribe("home/pico/dht11")
