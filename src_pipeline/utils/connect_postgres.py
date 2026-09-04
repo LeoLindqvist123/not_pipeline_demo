1ecd2909766c9d4f8e653f0d9617fd44e6af6d36
Leo Lindqvist Kröhnert <leolikro01@gmail.com>
2026-09-04 12:24:47 +0200

    grafana

--- src_pipeline/utils/connect_postgres.py
+++ src_pipeline/utils/connect_postgres.py
@@ -1,5 +1,5 @@
-import psycopg2
 import os
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,4 +9,14 @@ POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 HOST = os.getenv("DB_HOST", "localhost")
 
-DB_CONFIG = f"dbname={POSTGRES_DB} user={POSTGRES_USER} password={POSTGRES_PASSWORD}"
+DB_CONFIG = (
+    f"dbname={POSTGRES_DB} user={POSTGRES_USER} "
+    f"password={POSTGRES_PASSWORD} host={HOST}"
+)
+
+
+def query_db(sql, parameters: tuple = None):
+    with psycopg2.connect(DB_CONFIG) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, parameters)
+            return cur.fetchall() if cur.description else None
